08bba9c3bab534e1da300dfaa32c5496687a2f41
G-Levine <gabrael@gmail.com>
2024-06-10 00:21:28 -0700

    Fix display rotation and login credentials

--- provision.sh
+++ provision.sh
@@ -1,9 +1,27 @@
 #!/bin/bash -e
+DEFAULT_USER=pi
+mkdir -p /home/$DEFAULT_USER
 
 # Setup for Raspberry Pi 5
 echo 'dtparam=i2c_arm=on,i2c_arm_baudrate=400000' >> /boot/firmware/config.txt
 echo 'usb_max_current_enable=1' >> /boot/firmware/config.txt
+echo 'dtoverlay=waveshare-4dpic-3b' >> /boot/firmware/config.txt
+echo 'dtoverlay=waveshare-4dpic-4b' >> /boot/firmware/config.txt
+echo 'dtoverlay=waveshare-4dpic-5b' >> /boot/firmware/config.txt
+echo 'hdmi_force_hotplug=1' >> /boot/firmware/config.txt
+echo 'config_hdmi_boost=10' >> /boot/firmware/config.txt
+echo 'hdmi_group=2' >> /boot/firmware/config.txt
+echo 'hdmi_mode=87' >> /boot/firmware/config.txt
+echo 'hdmi_timings=720 0 100 20 100 720 0 20 8 20 0 0 0 60 0 48000000 6' >> /boot/firmware/config.txt
+echo 'start_x=0' >> /boot/firmware/config.txt
+echo 'gpu_mem=128' >> /boot/firmware/config.txt
 sed -i '1s/^/video=HDMI-A-1:720x720M@60D,rotate=270 /' /boot/firmware/cmdline.txt
+
+# Download and extract the display overlays
+curl 'https://files.waveshare.com/wiki/4inch%20HDMI%20LCD%20(C)/4HDMIB_DTBO.zip' -o 4HDMIB_DTBO.zip
+unzip 4HDMIB_DTBO.zip
+sudo cp 4HDMIB_DTBO/*.dtbo /boot/firmware/overlays/
+rm -r 4HDMIB_DTBO 4HDMIB_DTBO.zip
 
 # Set up avahi-daemon
 sudo apt install -y avahi-daemon
@@ -26,7 +44,7 @@ pip install --upgrade pyaudio deepgram-sdk
 
 # DepthAI
 sudo wget -qO- https://docs.luxonis.com/install_dependencies.sh | bash
-echo 'export OPENBLAS_CORETYPE=ARMV8' >> ~/.bashrc && source ~/.bashrc
+echo 'export OPENBLAS_CORETYPE=ARMV8' >> /home/$DEFAULT_USER/.bashrc && source /home/$DEFAULT_USER/.bashrc
 pip install blobconverter
 
 # Install ROS2
@@ -37,19 +55,19 @@ sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o
 sudo bash -c 'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null'
 sudo apt update && sudo apt install -y ros-dev-tools
 sudo apt install -y ros-jazzy-ros-base
-echo 'source /opt/ros/jazzy/setup.bash' >> ~/.bashrc
+echo 'source /opt/ros/jazzy/setup.bash' >> /home/$DEFAULT_USER/.bashrc
 source /opt/ros/jazzy/setup.bash
 
 # Create ROS2 workspace
-mkdir -p ~/ros2_ws/src
-cd ~/ros2_ws/src
+mkdir -p /home/$DEFAULT_USER/ros2_ws/src
+cd /home/$DEFAULT_USER/ros2_ws/src
 git clone https://github.com/G-Levine/control_board_hardware_interface.git
 git clone https://github.com/G-Levine/neural_controller.git --recurse-submodules 
 git clone https://github.com/G-Levine/pupper_v3_description.git
 git clone https://github.com/Nate711/pupper_feelings.git
 
 # Install dependencies
-cd ~/ros2_ws
+cd /home/$DEFAULT_USER/ros2_ws
 sudo apt install -y python3-colcon-common-extensions python3-rosdep
 sudo rosdep init && rosdep update
 rosdep install --from-paths src -y --ignore-src
@@ -59,11 +77,11 @@ sudo apt install -y ros-jazzy-ros2-control ros-jazzy-ros2-controllers ros-jazzy-
 
 # Build ROS2 workspace
 colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=RelWithDebInfo
-echo 'source ~/ros2_ws/install/setup.bash' >> ~/.bashrc
-source ~/ros2_ws/install/setup.bash
+echo 'source /home/$DEFAULT_USER/ros2_ws/install/setup.bash' >> /home/$DEFAULT_USER/.bashrc
+source /home/$DEFAULT_USER/ros2_ws/install/setup.bash
 
 # Install utils
-cd ~
+cd /home/$DEFAULT_USER
 git clone https://github.com/Nate711/utils.git -b launch_neural_controller
-bash ~/utils/install_battery_monitor.sh
-bash ~/utils/install_robot_auto_start_service.sh
+bash /home/$DEFAULT_USER/utils/install_battery_monitor.sh
+bash /home/$DEFAULT_USER/utils/install_robot_auto_start_service.sh
