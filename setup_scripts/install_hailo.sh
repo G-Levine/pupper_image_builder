b9878c7c95a8c5580f255e809456a4e3dc8dc954
Nathan Kau <nathankau@gmamil.com>
2024-11-03 02:06:58 +0900

    add fix for headers just for lolz

--- setup_scripts/install_hailo.sh
+++ setup_scripts/install_hailo.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-sudo apt install -y build-essential linux-headers-raspi
+sudo apt install -y build-essential linux-headers-*raspi
 pip install /home/pi/resources/hailort-4.19.0-cp312-cp312-linux_aarch64.whl
 pip install numpy Pillow supervision tqdm loguru
